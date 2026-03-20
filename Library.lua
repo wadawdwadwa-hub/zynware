9ee1570c6e79b1bb64e182c95905114039174069
wadawdwadwa-hub <jaydenmyers215@gmail.com>
2026-03-20 06:49:18 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -225,7 +225,7 @@
 	writefile("fff.ttf", game:HttpGet("https://github.com/i77lhm/storage/blob/refs/heads/main/fonts/ProggyClean.ttf"))
 
 	local tahoma = {
-		name = "SmallestPixel7",
+		name = "ProggyClean",
 		faces = {
 			{
 				name = "Regular",
