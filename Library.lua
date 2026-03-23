0f05b2ac8bfb8f5ffc3c4a76e41d4a002e3d0878
wadawdwadwa-hub <jaydenmyers215@gmail.com>
2026-03-23 04:41:30 -0400

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1207,7 +1207,7 @@
 				items.inventoryholder = library:create( "Frame" , {
 					Parent = items.LowContrast;
 					Name = "\0";
-					Position = dim2(0, 80, 0, 21);
+					Position = dim2(0, 95, 0, 28);
 					BorderColor3 = rgb(0, 0, 0);
 					Size = dim2(1, -80, 0, 0);
 					BorderSizePixel = 0;
