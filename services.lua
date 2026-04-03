777cf0bc3895dd13a41973a3eba9d667d8f40d17
wadawdwadwa-hub <jaydenmyers215@gmail.com>
2026-04-03 15:22:35 -0400

    Create services.lua

---
+++ services.lua
@@ -0,0 +1,48 @@
+local lib = {}
+lib.__index = lib
+
+
+function lib:GetService(self)
+    return cloneref(game:GetService(self))
+end
+
+
+function lib:CloneFunction(self)
+    return clonefunction(self)
+end
+
+
+function CloneReference(self)
+    return cloneref(self)
+end
+
+
+function lib:findfirstchild(self, value)
+    return self:FindFirstChild(value)
+end
+
+
+function lib:findfirstchildofclass(self, class)
+    return self:FindFirstChildOfClass(class)
+end
+
+
+function lib:wtvp(pos)
+    return Camera:WorldToViewportPoint(pos)
+end
+
+
+function lib:wtsp(pos)
+    return Camera:WorldToScreenPoint(pos)(pos)
+end
+
+
+function lib:framelimit(rendertime, deltatime, fps)
+    rendertime += deltatime
+	if (rendertime < 1 / fps) then
+		return
+	end
+	rendertime = 0
+end
+
+return lib
