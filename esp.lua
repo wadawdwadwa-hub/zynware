35bdc52b68b56625e90bad3403c35c8539c13a03
wadawdwadwa-hub <jaydenmyers215@gmail.com>
2026-04-04 00:47:51 -0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -116,7 +116,7 @@ function lib:DrawText(properties)
     obj.BorderSizePixel = 0
 
     obj.TextStrokeTransparency = 1
-	obj.FontFace = lib.SmallestPixel7
+	obj.FontFace = lib.ProggyClean
 
     obj.AnchorPoint = properties.AnchorPoint
     stroke.Parent = obj
